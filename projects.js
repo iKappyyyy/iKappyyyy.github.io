4b9cefd60a27e2bf28453c2195c4e9f1b65a8f25
iKappyyyy <108990210+iKappyyyy@users.noreply.github.com>
2024-01-20 23:16:40 +0200

    Re-Added files <3

---
+++ projects.js
@@ -0,0 +1,51 @@
+const projectsInfo = [
+  { folder: 'cutesy-ask-out', file: 'question.html', title: 'Will you go out with me?'},
+  { folder: 'rock-paper-scissors', file: 'rps.html', title: 'Rock Paper Scissors!' },
+  { folder: 'todo-list', file: 'todo-list.html', title: 'Todo List' },
+  { folder: 'youtube-recreation', file: 'youtube.html', title: 'YouTube!' },
+  { folder: 'stopwatch', file: 'stopwatch.html', title: 'Stopwatch' },
+  { folder: 'tic-tac-toe', file: 'tic-tac-toe.html', title: 'Tic Tac Toe!' },
+  { folder: 'colour-generator', file: 'colour-generator.html', title: 'Colour Generator!' }
+];
+const projectsElement = document.querySelector('.js-projects');
+
+let projectsHTML = '';
+let currentRow = '';
+let i = 0
+for (i = 0; i < projectsInfo.length; i++) {
+  if (i % 3 === 0) {
+    currentRow += '</div>';
+    projectsHTML += currentRow;
+    currentRow = '<div class="row">';
+  }
+  currentRow += `
+  <div class="js-project project">
+    <div class="project-title">
+      ${projectsInfo[i].title}
+    </div>
+    <img class="project-thumbnail" src="${projectsInfo[i].folder}/thumbnail.png">
+  </div>
+  `
+}
+while (i % 3 !== 0) {
+  currentRow += `
+  <div class="project">
+    <div class="project-title">
+      To Be Done.
+    </div>
+    <img class="project-thumbnail" src="thumbnail.png">
+  </div>
+  `
+  i++;
+}
+
+currentRow += '</div>';
+projectsHTML += currentRow;
+currentRow = '';
+projectsElement.innerHTML = projectsHTML;
+const projects = document.querySelectorAll('.js-project');
+projectsInfo.forEach((projectObject, i) => {
+  projects[i].addEventListener('click', () => {
+    document.location.href = `${projectObject.folder}/${projectObject.file}`;
+  });
+});
