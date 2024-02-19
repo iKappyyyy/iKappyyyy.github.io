1736faa286f3a3d76901ec2240585e13aac2cf03
iKappyyyy <108990210+iKappyyyy@users.noreply.github.com>
2024-02-19 10:44:17 +0200

    Update text.js

--- countdown/scripts/text.js
+++ countdown/scripts/text.js
@@ -1,5 +1,5 @@
 const eventTextElement = document.querySelector('.js-event');
-const eventText = 'I\'m gonna die LOL hehe haha';
+const eventText = 'my birthday!';
 eventTextElement.addEventListener('click', () => {
   window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
 });
