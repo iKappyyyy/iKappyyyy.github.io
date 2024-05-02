251b2182fdb4343d68bf6a6246873fd1aa9acbb0
iKappyyyy <specialtimewithdaniel@gmail.com>
2024-05-02 07:32:44 +0300

    finished blur range

--- pointless-button/scripts/main.js
+++ pointless-button/scripts/main.js
@@ -6,16 +6,18 @@ import { toggleAchievementMenu, achievementReached, updateAchievementLists, crea
 import { toggleBackgroundsMenu, loadObtainedBackgrounds, backgroundReached, updateBackgroundsLists, changeBackground, createBackgroundPopUpElement } from "./backgrounds.js";
 import { changeMuteIcon, saveMutedToLocalStorage } from "./mute.js";
 import { achievementPopUpDurationMs, backgroundPopUpDurationMs, playPopUpAnimation, popUpElementHeightPx } from "./animations.js";
-import { toggleSettingsMenu } from "./settings.js";
+import { toggleSettingsMenu, changeBackgroundBlur } from "./settings.js";
 
 const button = document.querySelector('.js-pointless-button');
 const achievementButton = document.querySelector('.js-achievement-button');
 const backgroundButton = document.querySelector('.js-background-button');
 const settingsButton = document.querySelector('.js-settings-button');
 const muteButton = document.querySelector('.js-mute-button');
+const backgroundBlurRange = document.querySelector('.js-background-blur-range');
 const backgroundImage = localStorage.getItem('backgroundImage') || './images/background-1.gif';
 let clicks = Number(localStorage.getItem('clicks')) || 0;
 let audioMuted = Number(localStorage.getItem('muted')) || 0;
+let backgroundBlur = Number(localStorage.getItem('backgroundBlur')) || 10;
 let achievementMenuToggled = false;
 let backgroundsMenuToggled = false;
 let settingsMenuToggled = false;
@@ -26,6 +28,8 @@ loadObtainedAchievements(obtainedAchievements); // load obtained achievements at
 loadObtainedBackgrounds(obtainedBackgrounds); // load obtained backgrounds at the start of program
 changeMuteIcon(muteButton, audioMuted); // load mute icon at the start of program
 changeBackground(backgroundImage); // load background at the start of program
+changeBackgroundBlur(backgroundBlur); // load background blur at the start of program
+backgroundBlurRange.value = backgroundBlur;
 
 button.addEventListener('click', () => {
   if (!audioMuted) playClickSound();
@@ -95,3 +99,8 @@ muteButton.addEventListener('click', () => {
   changeMuteIcon(muteButton, audioMuted);
   saveMutedToLocalStorage(audioMuted);
 });
+
+backgroundBlurRange.addEventListener('input', (event) => {
+  const rangeValue = event.target.value;
+  changeBackgroundBlur(rangeValue);
+})
--- pointless-button/scripts/settings.js
+++ pointless-button/scripts/settings.js
@@ -1,3 +1,6 @@
+const minBlur = 0;
+const maxBlur = 20;
+
 export function toggleSettingsMenu(settingsMenuToggled) {
   const menu = document.querySelector('.js-settings-menu');
   const icon = document.querySelector('.js-settings-icon');
@@ -11,4 +14,22 @@ export function toggleSettingsMenu(settingsMenuToggled) {
     icon.style.transform = 'rotate(75deg)';
     return true;
   }
+}
+
+export function changeBackgroundBlur(blurPx) {
+  if (blurPx < minBlur || maxBlur < blurPx) return;
+
+  document.body.style.backdropFilter = `blur(${blurPx}px)`;
+  changeBackgroundBlurText(blurPx);
+  saveBlurToLocalStorage(blurPx);
+}
+
+function saveBlurToLocalStorage(blurPx) {
+  localStorage.setItem('backgroundBlur', String(blurPx));
+}
+
+function changeBackgroundBlurText(blurPx) {
+  const backgroundBlurText = document.querySelector('.js-background-blur-amount');
+
+  backgroundBlurText.innerHTML = String(blurPx);
 }
