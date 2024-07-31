6fb35d035267cdf8f7a8f5ddfee0008b086b42b8
iKappyyyy <108990210+iKappyyyy@users.noreply.github.com>
2024-07-31 15:41:53 +0300

    added juvsoccerlayout
    yippee

---
+++ juvSoccerLayout/scripts/data/playerInfos.js
@@ -0,0 +1,56 @@
+const playerInfos = {
+  '1': {
+    playerName: 'ניסים מזרכי',
+    playerImage: 'chose'
+  },
+  '2': {
+    playerName: 'אדרי בן זכאי',
+    playerImage: 'chose'
+  },
+  '3': {
+    playerName: 'שרק',
+    playerImage: 'chose'
+  },
+  '4': {
+    playerName: 'פטריק סטאר',
+    playerImage: 'chose'
+  },
+  '5': {
+    playerName: 'מגנוק',
+    playerImage: 'chose'
+  },
+  '6': {
+    playerName: 'טום טרגר',
+    playerImage: 'chose'
+  },
+  '7': {
+    playerName: 'אור פז',
+    playerImage: 'chose'
+  },
+  '8': {
+    playerName: 'גרי המבורגרי',
+    playerImage: 'chose'
+  },
+  '9': {
+    playerName: 'אסף לוץ',
+    playerImage: 'chose'
+  },
+  '10': {
+    playerName: 'אבי רון',
+    playerImage: 'chose'
+  },
+  '11': {
+    playerName: 'יערון',
+    playerImage: 'chose'
+  },
+  '12': {
+    playerName: 'יצחקי מצחקי',
+    playerImage: 'chose'
+  },
+  '13': {
+    playerName: 'פיתר פרכר',
+    playerImage: 'chose'
+  }
+}
+
+export default playerInfos;
